f459ca345fc8878e306388c9cda89f28bd9ecdd9
James-Belle <jamesbelle549@gmail.com>
2025-10-07 11:28:18 -0400

    import template with audio

---
+++ TerminalGameWithAudio/TerminalGame.cs
@@ -0,0 +1,29 @@
+﻿using Raylib_cs;
+using System;
+
+namespace MohawkTerminalGame
+{
+    public class TerminalGame
+    {
+        // Place your variables here
+        
+       
+        /// Run once before Execute begins
+        public void Setup()
+        {
+            // Program configuration
+            
+            
+        }
+
+        // Execute() runs based on Program.TerminalExecuteMode (assign to it in Setup).
+        //  ExecuteOnce: runs only once. Once Execute() is done, program closes.
+        //  ExecuteLoop: runs in infinite loop. Next iteration starts at the top of Execute().
+        //  ExecuteTime: runs at timed intervals (eg. "FPS"). Code tries to run at Program.TargetFPS.
+        //               Code must finish within the alloted time frame for this to work well.
+        public void Execute()
+        {
+
+        }
+    }
+}
